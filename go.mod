6196527e43a77bac3e6d66664d0981b03125b0e5
wjqserver <114663932+WJQSERVER@users.noreply.github.com>
2025-06-04 20:56:51 +0800

    use reco

--- go.mod
+++ go.mod
@@ -2,16 +2,15 @@ module github.com/fenthope/record
 
 go 1.24.3
 
-require (
-	github.com/WJQSERVER-STUDIO/logger v1.7.2
-	github.com/infinite-iroha/touka v0.0.5
-)
+require github.com/infinite-iroha/touka v0.0.6
 
 require (
 	github.com/WJQSERVER-STUDIO/go-utils/copyb v0.0.4 // indirect
-	github.com/WJQSERVER-STUDIO/go-utils/log v0.0.3 // indirect
 	github.com/WJQSERVER-STUDIO/httpc v0.5.1 // indirect
+	github.com/fenthope/reco v0.0.1 // indirect
 	github.com/go-json-experiment/json v0.0.0-20250517221953-25912455fbc8 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
+
+//replace github.com/infinite-iroha/touka => /data/github/WJQSERVER/touka
--- record.go
+++ record.go
@@ -3,23 +3,14 @@ package record
 import (
 	"time"
 
-	"github.com/WJQSERVER-STUDIO/logger"
 	"github.com/infinite-iroha/touka"
-)
-
-var (
-	logw       = logger.Logw
-	logDump    = logger.LogDump
-	logDebug   = logger.LogDebug
-	logInfo    = logger.LogInfo
-	logWarning = logger.LogWarning
-	logError   = logger.LogError
 )
 
 // 日志中间件
 // 请保证logger实例被定义
 func Middleware() touka.HandlerFunc {
 	return func(c *touka.Context) {
+		logger := c.GetLogger()
 		startTime := time.Now()
 
 		c.Next()
@@ -27,6 +18,6 @@ func Middleware() touka.HandlerFunc {
 		endTime := time.Now()
 		timingResults := endTime.Sub(startTime)
 
-		logInfo("%s %s %s %s %s %d %s ", c.ClientIP(), c.Request.Method, c.GetProtocol(), c.Request.URL.Path, c.Request.UserAgent(), c.Writer.Status(), timingResults)
+		logger.Infof("%s %s %s %s %s %d %s ", c.ClientIP(), c.Request.Method, c.GetProtocol(), c.Request.URL.Path, c.Request.UserAgent(), c.Writer.Status(), timingResults)
 	}
 }
